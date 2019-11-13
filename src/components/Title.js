7432d07f6be25bf4c07a7cee5b6bc2067fc9629c
Diego Sagredo <dsagredog@gmail.com>
2019-11-12 22:17:14 -0300

    modifica readme

--- src/components/Title.js
+++ src/components/Title.js
@@ -7,7 +7,7 @@ export const Title = () => {
             <Row>
                 <Col xs="12">
                     <Jumbotron className="text-center">
-                        <h1 className="display-5">TO-DO Listado</h1>
+                        <h1 className="display-5">To-do lista de cosas por hacer</h1>
                     </Jumbotron>
                 </Col>
             </Row>
